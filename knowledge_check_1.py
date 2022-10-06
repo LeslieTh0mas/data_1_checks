38175d3ece8d1e951991bcb164244b141b25d00b
LeslieTh0mas <email.leslie.thomas@gmail.com>
2022-10-05 20:58:02 -0400

    first commit

---
+++ knowledge_check_1.py
@@ -0,0 +1,20 @@
+print ("hello world!")
+type_of_account= ['advisor', 'retail', 'correspondent']
+print(type_of_account[2])
+states = {"OH": "Ohio", "KY": "Kentucky"}
+print(states['OH'])
+var= ('1', '2', '3', '4')
+print("Value in Var[0] = ", var[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
